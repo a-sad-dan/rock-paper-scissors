334276fc732416a3cc72fbdd90e93a8f98cf8621
a-sad-dan <danishasad91@gmail.com>
2022-11-13 14:22:44 +0530

    Change main heading font

--- script.js
+++ script.js
@@ -5,49 +5,83 @@ function getComputerChoice() {
     return (computerSelection);
 }
 
+var computerScore = document.querySelector("#computer-score");
+var playerScore = document.querySelector("#player-score");
+
 //Function to play one round of the game
 function playRound(playerSelection, computerSelection) {
+
+    // Select Image frame and apply the proper image
+    const playerFrame = document.querySelector("#playerChoiceImage");
+    const computerFrame = document.querySelector("#computerChoiceImage");
+    playerFrame.src = `Assets/${playerSelection}.png`;
+    computerFrame.src = `Assets/${computerSelection}.png`;
+
+
+    //  CASES
     // Tie Case
     if (playerSelection == computerSelection) {
-        return -1;
+
     }
     // Win cases for the player
-    else if (playerSelection === 'rock' && computerSelection === 'scissors') {return 1;}
-     else if (playerSelection === 'paper' && computerSelection === 'rock') {return 1;}
-     else if (playerSelection === 'scissors' && computerSelection === 'paper') {return 1;}
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore.textContent = `${parseInt(playerScore.textContent) + 1}`;
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore.textContent = `${parseInt(playerScore.textContent) + 1}`;
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore.textContent = `${parseInt(playerScore.textContent) + 1}`;
+    }
 
     //Lose case for the player
     else {
-        return 0;
+        computerScore.textContent = `${parseInt(computerScore.textContent) + 1}`;
     }
+
+    if (computerScore.textContent >= '5' || playerScore.textContent >= '5') {
+        // document.querySelector('#versus-text').textContent ='Done';
+        setTimeout(() => {
+            toggleModal()
+        }, 600);
+        if (computerScore.textContent > playerScore.textContent) {
+            document.querySelector('#result-image').src = '/Assets/cry.png';
+            document.querySelector('#result-statement').textContent = 'You Lose!';
+        }
+        if (computerScore.textContent < playerScore.textContent) {
+            document.querySelector('#result-image').src = '/Assets/trophy-golden.png';
+            document.querySelector('#result-statement').textContent = 'You Win!';
+        }
+    }
+
 }
+// Function to toggle Modal
+function toggleModal() {
+    document.querySelector('#modal').classList.toggle('hidden');
+    document.querySelector('#main-content').classList.toggle('hidden');
+}
+
+// Function to restart
+function resetGame() {
+    toggleModal();
+    computerScore.textContent = `0`;
+    playerScore.textContent = `0`;
+}
+
+document.querySelector('#replay').addEventListener('click', () => resetGame());
 
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
-const playerFrame = document.querySelector("#playerChoiceImage")
-const computerFrame = document.querySelector("#computerChoiceImage")
+// Playing correct choice on click
+rock.addEventListener('click', () => playRound('rock', getComputerChoice()));
+paper.addEventListener('click', () => playRound('paper', getComputerChoice()));
+scissors.addEventListener('click', () => playRound('scissors', getComputerChoice()));
 
 
-rock.addEventListener('click', () => {
-    changeImage(playerFrame, 'rock');
-    changeImage(computerFrame,getComputerChoice());
-});
-paper.addEventListener('click', () => {
-    changeImage(playerFrame, 'paper');
-    changeImage(computerFrame,getComputerChoice());
-});
-scissors.addEventListener('click', () => {
-    changeImage(playerFrame, 'scissors');
-    changeImage(computerFrame,getComputerChoice());
-});
+// Adding sounds to the website
+rock.addEventListener('click', () => document.querySelector("#rock-fx").play());
+paper.addEventListener('click', () => document.querySelector("#paper-fx").play());
+scissors.addEventListener('click', () => document.querySelector("#scissors-fx").play());
 
-
-
-
-// Function to change the image shown in selected window
-
-function changeImage(elementName, imageName) {
-    elementName.src = `Assets/${imageName}.png`;
-}
